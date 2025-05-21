e424fb1dcf43c9385f2b77e8a242351f9e07c79c
prostatus <mel.sirvania@gmail.com>
2025-05-21 09:21:50 +0300

    Script upd

--- plot_results.py
+++ plot_results.py
@@ -9,12 +9,21 @@ bandwidth = data[:, 2]
 # Переводим размер вектора в мегабайты (2 вектора * 8 байт)
 size_MB = sizes * 2 * 8 / (1024**2)
 
-plt.plot(size_MB, gflops, label="GFLOPS")
-plt.plot(size_MB, bandwidth, label="Bandwidth (GB/s)")
-plt.xlabel("Vector Size (MB)")
-plt.ylabel("Performance")
-plt.legend()
-plt.grid()
-plt.title("Performance vs Vector Size")
+plt.figure(figsize=(12, 8))
+plt.plot(size_MB, gflops, label="GFLOPS", marker='o')
+plt.plot(size_MB, bandwidth, label="Bandwidth", marker='x')
+
+plt.xlabel("Vector Size (MB)", fontsize=18)
+plt.ylabel("Performance (GFLOPS) / Bandwidth (GB/s)", fontsize=18)
+plt.title("Scalar Vector Product Performance vs Vector Size", fontsize=18)
+plt.legend(fontsize=18)
+plt.grid(True, which="both", ls="--", linewidth=0.5)
+
+plt.yscale("log")
+plt.xscale("log")
+
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+
 plt.tight_layout()
 plt.show()
